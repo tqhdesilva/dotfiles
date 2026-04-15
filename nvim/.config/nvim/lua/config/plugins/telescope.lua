0d982cf41eb73860ba6baea04201f3c78eed6c4b
Tyrone DeSilva <tdesilvaqihao@gmail.com>
2026-04-14 23:13:02 -0700

    update find, find all binds

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -16,13 +16,15 @@ return {
     })
     telescope.load_extension("fzf")
 
-    vim.keymap.set("n", "<leader>ff", function()
-      builtin.find_files({ hidden = true })
-    end, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+    vim.keymap.set("n", "<leader>fF", function()
+      builtin.find_files({ hidden = true, no_ignore = true })
+    end, { desc = "Find files (all)" })
     vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
-    vim.keymap.set("n", "<leader>fs", function()
-      builtin.live_grep({ additional_args = { "--hidden" } })
-    end, { desc = "Live grep" })
+    vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Live grep" })
+    vim.keymap.set("n", "<leader>fS", function()
+      builtin.live_grep({ additional_args = { "--hidden", "--no-ignore" } })
+    end, { desc = "Live grep (all)" })
     vim.keymap.set("n", "<leader>fc", builtin.grep_string, { desc = "Grep word under cursor" })
   end,
 }
